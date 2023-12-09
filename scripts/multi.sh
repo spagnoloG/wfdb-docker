b99cbcbb75d7a9dd7c4d3aedcd4b4048739b545e
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2023-12-09 18:17:15 +0100

    Add script to run detector on whole dataset

---
+++ scripts/multi.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+
+set -eux
+
+if [ -f "eval1.txt" ]; then
+    rm eval1.txt
+fi
+
+if [ -f "eval2.txt" ]; then
+    rm eval2.txt
+fi
+
+
+FILES="*.dat"
+
+
+for f in $FILES
+do
+  f=$(basename $f)
+  f=${f%.*}
+
+  echo  $f 
+  ./qrs_detect -r $f
+
+  docker run -it --rm -v "$(pwd)":/data ghcr.io/spagnolog/wfdb-docker:main bxb -r "/data/$f" -a atr qrs -l eval1.txt eval2.txt
+ done
+
+docker run -it --rm -v "$(pwd)":/data ghcr.io/spagnolog/wfdb-docker:main sumstats eval1.txt eval2.txt >results.txt 
